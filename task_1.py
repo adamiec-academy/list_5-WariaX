0002ce821bd347f3f8ac74be585cef9071bf3753
WariaX <115689237+WariaX@users.noreply.github.com>
2023-01-02 13:30:10 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,23 @@
-def longest_word():
-    return
+def words_from_file(file):
+    data = []
+    for x in open(file, encoding="utf-8"):
+        data.append(x.strip().split(4 * " "))
+        
+    return data
+
+
+def longest_word(words): 
+    result_max = words[0]  
+    for word in words: 
+        if len(word) > len(result_max):
+            result_max = word  
+
+    
+    
+    return result_max
+
+
+words = words_from_file("words.txt")
+
+
+print(longest_word(words))
