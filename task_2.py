354eb17a22347201a8609d801b47ea69ce41c828
WariaX <115689237+WariaX@users.noreply.github.com>
2023-01-04 11:11:55 +0100

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,7 +1,11 @@
 def unique(data):
-    zbior = set(data) 
+    zbior = set(data) #([4, 2, 3, 1])
     lista = [] 
 
-    for x in zbior:
-        lista.append(x)
+    for x in data:
+        if x not in lista:
+            lista.append(x)
+
+
+
     return lista
